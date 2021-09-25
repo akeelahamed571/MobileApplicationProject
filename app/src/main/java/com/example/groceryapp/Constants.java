eecd19a3dbe37bd0751c137088ceb9da8e313e12
akeel <it20068264@my.sliit.lk>
2021-09-26 00:13:01 +0530

    the first commit

---
+++ app/src/main/java/com/example/groceryapp/Constants.java
@@ -0,0 +1,23 @@
+package com.example.groceryapp;
+
+public class Constants {
+
+    public static final String FCM_KEY = "";
+    public static final String FCM_TOPIC = "PUSH_NOTIFICATIONS";
+    //product categories
+
+    public static final String [] productOptions = {
+      "Beverages","Beauty & personal Care",
+      "Baby Kids","Biscuit Snake","Cooking Needs",
+      "Fruits","Frozen Food","Pet","Pharmacy",
+      "Vegetables","Others"
+    };
+
+    public static final String [] productOptions1 = {
+            "All",
+            "Beverages","Beauty & personal Care",
+            "Baby Kids","Biscuit Snake","Cooking Needs",
+            "Fruits","Frozen Food","Pet","Pharmacy",
+            "Vegetables","Others"
+    };
+}
