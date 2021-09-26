55802f86af6d71142ca9b0d5d540715994100db3
Abdul <ax420r@gmail.com>
2021-09-26 14:23:13 +0530

    Successfully commited my function managin promotion codes all layouts and java files and acticities perfectly done!!

---
+++ app/src/main/java/com/example/functionpromocode/Activity/Buyer/EditUserActivity.java
@@ -0,0 +1,16 @@
+package com.example.functionpromocode.Activity.Buyer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.groceryapp.R;
+
+public class EditUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user);
+    }
+}
